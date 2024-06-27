89ed8d1933c95a97f267207a57fd6568059d9067
Davichobits <davidruizdeveloper@gmail.com>
2024-06-27 06:06:06 -0500

    Physics added

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { Game as MainGame } from './scenes/Game';
-import { AUTO, Scale,Game } from 'phaser';
+import { AUTO, Scale, Game } from 'phaser';
 
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
@@ -9,6 +9,13 @@ const config = {
     height: 768,
     parent: 'game-container',
     backgroundColor: '#028af8',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
     scale: {
         mode: Scale.FIT,
         autoCenter: Scale.CENTER_BOTH
--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -14,8 +14,15 @@ export class Game extends Scene {
     }
 
     create() {
+        // Background
         this.add.image(0, 0, 'background').setOrigin(0);
-        this.add.image(0, this.game.config.height, 'ground').setOrigin(0, 1)
-        this.add.image(0, 300, 'platform').setOrigin(0);
+        // Ground
+        this.physics.add.staticImage(0, this.game.config.height, 'ground').setOrigin(0, 1)
+        // platforms
+        this.platforms = this.physics.add.staticGroup();
+        this.platforms.create(0, 300, 'platform');
+        this.platforms.create(600, 400, 'platform');
+        this.platforms.create(50, 250, 'platform');
+        this.platforms.create(750, 220, 'platform');
     }
 }
